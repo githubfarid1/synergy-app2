89f50e838e05c597ad8e765b9691c9b6e7ed644e
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-05-04 22:01:59 +0900

    update script

--- main.py
+++ main.py
@@ -228,10 +228,7 @@ class MainFrame(ttk.Frame):
 		super().__init__(window)
 		# configure
 		# self.grid(column=0, row=0, sticky=(N, E, W, S), columnspan=2)
-		s = ttk.Style()
-		s.configure('My.TFrame', background='green')
-
-		self.config(padding="20 20 20 20", borderwidth=1, relief='groove', style='My.TFrame')
+		self.config(padding="20 20 20 20", borderwidth=1, relief='groove')
 		# self.place(anchor=CENTER)
 		self.columnconfigure(0, weight=1)
 		self.columnconfigure(1, weight=1)
