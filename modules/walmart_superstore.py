11c568888071a47610db4a483b45bb7d62ff8a53
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-05-28 22:15:23 +0900

    update script

--- modules/walmart_superstore.py
+++ modules/walmart_superstore.py
@@ -75,6 +75,7 @@ def get_urls(xlsheet, domainwl=[], cost_empty_only=False):
             if (domain, suffix) in domainwl:
                 tpl = (url, i)
                 if cost_empty_only == True:
+                    input(xlsheet[f'B{i}'].value)
                     if xlsheet[f'B{i}'].value == None or xlsheet[f'B{i}'].value == 'None':
                         urlList.append(tpl)
                 else:
