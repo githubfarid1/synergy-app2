b355fe521cacc3ce92d5e8cb372f0995c5ed8b74
farid <Muhammad Farid Saleh>
2023-06-28 23:20:05 +0900

    update script

--- tester/statistic_tester.py
+++ tester/statistic_tester.py
@@ -1,27 +1,28 @@
 import requests
 
 cookies = {
-    'session-id': '143-3445911-9496428',
-    'i18n-prefs': 'USD',
-    'ubid-main': '135-1698517-0362129',
-    's_pers': '%20s_fid%3D71A5BA2BB54387EC-23184C6241077C36%7C1843500634686%3B%20s_dl%3D1%7C1685649634687%3B%20s_ev15%3D%255B%255B%2527NSGoogle%2527%252C%25271685647834690%2527%255D%255D%7C1843500634690%3B',
-    'sid': '"dX8/EHJKQIXIjQiHhXasMw==|WqlaeXcqyUHpafXiPOE+HvuAXb9VtooobpWbpBGPdRw="',
+    'ubid-main': '134-2916247-3956466',
+    'sid': '"RKuvscDzYqo2pj6EC6xZSg==|rFEMF0Ui1EtgdZNsSaK0if3w96U0CrXu+BuN5pNtC8w="',
     '__Host-mselc': 'H4sIAAAAAAAA/6tWSs5MUbJSSsytyjPUS0xOzi/NK9HLT85M0XM0DA0KDncOcgly8bWMUNJRykVSmZtalJyRCFKq52jkZ+pp6GTi6Gvq5OkGUpeNrLAApCQkLMDF29M7wsDFNUipFgAKAR4EdQAAAA==',
-    'csm-hit': 'tb:PX1964Z7C79TD2WTG6SF+s-PX1964Z7C79TD2WTG6SF|1686870448919&t:1686870448919&adb:adblk_no',
-    'x-main': '"gtbOEYZN@OUowIfunUGsjiVHUzltTTkVWqILyfswxxe2ajrdhVUiZ7gHsPyMs6rM"',
-    'at-main': 'Atza|IwEBIGhTt9SC20bEDL_jPZ-uWJ8eOPMFcghh2361jPFWp6na_i0MegZBxkkd41Bpp8WrKxNWtcRZfmF2S9dt8P-EoGpqCqnjV2mDjzd3SCjaaYhSSIprXyykVKpB9l0ZV4ygu5t4XwMc69tPZcbVl9wauMC7jGNhwoYfUtyVifeFjGgqicfSUGwaLOI40Cr_yHMzasSZ-QypwZ6Dd_mRAkt8oHTQqTZePHDMM0-AthhN2otI6Q',
-    'sess-at-main': '"sWzNJufEovEvfA5e7MN4evAhPItNf7P30NyYRq6arZQ="',
-    'sst-main': 'Sst1|PQEgEQOY7GkT5bLupUDyHd_JCZ0-Y3uGRb_jlkpn-M3PdlFyQ4xQD2LZM5TN_NBF0qOg9tUDb0-BdOnHkDhUfTfkGABaziFvpQ2RoOKsFz7nSCLlz32DlqBFns5La5L_GC5pc1lfEB1v_AHnIc45fzMxGC-o2UqGSxaTYiGDrVNYfcBecWCei0CeafmHa1P9LwRLN__lTQ2KzEKIVjlS4mBIZdJF_mk7wR7qB7jWD2-qigz4ATyuzzEtNcvKzMXolMMvtp0xbODE8_KHtFlJNlMB2JCiUQ1vnHKCm-xQnUMOmug',
-    'session-id-time': '2082787201l',
-    'session-token': 'laGVoAf8uOh/IUcntb9QA/S4yyT0pqgo1N+C/TwehlQMpZetIa0yFWCy3fVMdnjEYy/phx3QJtCL/9TUdbb3CMwNzLK5D1RrqrUuS1CSTdRJ7AooIEG4MLvF0cNHroH6p58LMBV2oUAxUNmNe33XnrP+lMs6AHHvp+vGtp1pnFYTad67LTPPY0hZcbfH/iudg96f/VPd0oomop/govpyPpng2VY8wctn3HlYGPL7m+pQsavM2kA+4xFa2dgyWNU3k0gZXBW99gA=',
+    'lc-main': 'en_US',
+    'i18n-prefs': 'USD',
+    'session-id': '131-7427739-0234048',
+    'sp-cdn': '"L5Z9:CA"',
+    'csm-hit': 'tb:BCSM1CGNC5V9WNVAWCFE+s-BCSM1CGNC5V9WNVAWCFE|1687961922928&t:1687961922928&adb:adblk_no',
+    'session-id-time': '2318681929l',
+    'session-token': 'OSAM/q5JNzWa4x6Bs4URSqrtmkMcrO+NDrHx96DKpeXv/gK4zBbozK43JwOCAQYJfym4WKBU7vtj50r3+se0la5ABj7PuzHIIEKNc6kiuebU6ft3lnAg/cBmIkTJ47gYn2ow+nTDIQyW3+A5Juc/iTwuJjCdoKUlY/3t7AkIds7BhHT7jnvO89sqPzcGfkZwQXQl4zT3RhsmSqkXi6Skd1qPEsjXv5VyjEKJHCaXMd2WqvJa5ORiTx74kEdLxMBk',
+    'x-main': '"dTPGR1EUeOl0rlwt6KYxgWgy@y79zqGILIqaEwhJZJFeO8DwqwiEUwoJHIt9cdD1"',
+    'at-main': 'Atza|IwEBIOYwB3Vw6gSC-9mrn55LRLkEb0EM6phy53Pf-yBPU-gIp3NpvkPqV66XMcrULpL-D95n_V0K8y3utdhHU-Kjsycc-hGCVuiOZeV5ITbGjdXoCHzjAj5ykd8_Ydu1XQPf0zaZQoDpCuxljuuNhAKxAdkQxHU_9AC1PwXMKX0IFM1jmUqQhFI8HBd1Pfdrs9mV669wPW8zRwN2ETDIbBOqLQAS_gDLtXke7rpW5F3Bep_DQw',
+    'sess-at-main': '"ZDpcJt3EExmE+bz3zjzLyOFjPxJEGGF6IL0BaaOuPHU="',
+    'sst-main': 'Sst1|PQFl5t7iExTeRMvRjVqBF5t-CSMhh_H1suyQH_6sq0wbPbSp7fL5y2iGCApDjRauZFPSdgzV6epj77x_BnUAfaxnLK6k31ZhTLJbYVTgj-gexZtRFpuqc8XJ_4erx_DODYlWYnyXg_L1G5tVXNu-KXIrPz8m9DstbqGn6soXNdt3A1LxOhi1rpJdYBxgDVRoULtQQQBCw2akA6CcIXo5vLkVO_C1V1cTaxat6lw2rpxLVHIb6oshSnFoIlxiub9IkVkBbQlH1VbGR6RmGidkf9-B42sIQL9vtWI-IiCNVGzdNjE',
 }
 
 headers = {
     'authority': 'sellercentral.amazon.com',
     'accept': '*/*',
     'accept-language': 'en-US,en;q=0.9',
-    # 'cookie': 'session-id=143-3445911-9496428; i18n-prefs=USD; ubid-main=135-1698517-0362129; s_pers=%20s_fid%3D71A5BA2BB54387EC-23184C6241077C36%7C1843500634686%3B%20s_dl%3D1%7C1685649634687%3B%20s_ev15%3D%255B%255B%2527NSGoogle%2527%252C%25271685647834690%2527%255D%255D%7C1843500634690%3B; sid="dX8/EHJKQIXIjQiHhXasMw==|WqlaeXcqyUHpafXiPOE+HvuAXb9VtooobpWbpBGPdRw="; __Host-mselc=H4sIAAAAAAAA/6tWSs5MUbJSSsytyjPUS0xOzi/NK9HLT85M0XM0DA0KDncOcgly8bWMUNJRykVSmZtalJyRCFKq52jkZ+pp6GTi6Gvq5OkGUpeNrLAApCQkLMDF29M7wsDFNUipFgAKAR4EdQAAAA==; csm-hit=tb:PX1964Z7C79TD2WTG6SF+s-PX1964Z7C79TD2WTG6SF|1686870448919&t:1686870448919&adb:adblk_no; x-main="gtbOEYZN@OUowIfunUGsjiVHUzltTTkVWqILyfswxxe2ajrdhVUiZ7gHsPyMs6rM"; at-main=Atza|IwEBIGhTt9SC20bEDL_jPZ-uWJ8eOPMFcghh2361jPFWp6na_i0MegZBxkkd41Bpp8WrKxNWtcRZfmF2S9dt8P-EoGpqCqnjV2mDjzd3SCjaaYhSSIprXyykVKpB9l0ZV4ygu5t4XwMc69tPZcbVl9wauMC7jGNhwoYfUtyVifeFjGgqicfSUGwaLOI40Cr_yHMzasSZ-QypwZ6Dd_mRAkt8oHTQqTZePHDMM0-AthhN2otI6Q; sess-at-main="sWzNJufEovEvfA5e7MN4evAhPItNf7P30NyYRq6arZQ="; sst-main=Sst1|PQEgEQOY7GkT5bLupUDyHd_JCZ0-Y3uGRb_jlkpn-M3PdlFyQ4xQD2LZM5TN_NBF0qOg9tUDb0-BdOnHkDhUfTfkGABaziFvpQ2RoOKsFz7nSCLlz32DlqBFns5La5L_GC5pc1lfEB1v_AHnIc45fzMxGC-o2UqGSxaTYiGDrVNYfcBecWCei0CeafmHa1P9LwRLN__lTQ2KzEKIVjlS4mBIZdJF_mk7wR7qB7jWD2-qigz4ATyuzzEtNcvKzMXolMMvtp0xbODE8_KHtFlJNlMB2JCiUQ1vnHKCm-xQnUMOmug; session-id-time=2082787201l; session-token=laGVoAf8uOh/IUcntb9QA/S4yyT0pqgo1N+C/TwehlQMpZetIa0yFWCy3fVMdnjEYy/phx3QJtCL/9TUdbb3CMwNzLK5D1RrqrUuS1CSTdRJ7AooIEG4MLvF0cNHroH6p58LMBV2oUAxUNmNe33XnrP+lMs6AHHvp+vGtp1pnFYTad67LTPPY0hZcbfH/iudg96f/VPd0oomop/govpyPpng2VY8wctn3HlYGPL7m+pQsavM2kA+4xFa2dgyWNU3k0gZXBW99gA=',
-    'referer': 'https://sellercentral.amazon.com/revcal?ref=RC1&',
+    # 'cookie': 'ubid-main=134-2916247-3956466; sid="RKuvscDzYqo2pj6EC6xZSg==|rFEMF0Ui1EtgdZNsSaK0if3w96U0CrXu+BuN5pNtC8w="; __Host-mselc=H4sIAAAAAAAA/6tWSs5MUbJSSsytyjPUS0xOzi/NK9HLT85M0XM0DA0KDncOcgly8bWMUNJRykVSmZtalJyRCFKq52jkZ+pp6GTi6Gvq5OkGUpeNrLAApCQkLMDF29M7wsDFNUipFgAKAR4EdQAAAA==; lc-main=en_US; i18n-prefs=USD; session-id=131-7427739-0234048; sp-cdn="L5Z9:CA"; csm-hit=tb:BCSM1CGNC5V9WNVAWCFE+s-BCSM1CGNC5V9WNVAWCFE|1687961922928&t:1687961922928&adb:adblk_no; session-id-time=2318681929l; session-token=OSAM/q5JNzWa4x6Bs4URSqrtmkMcrO+NDrHx96DKpeXv/gK4zBbozK43JwOCAQYJfym4WKBU7vtj50r3+se0la5ABj7PuzHIIEKNc6kiuebU6ft3lnAg/cBmIkTJ47gYn2ow+nTDIQyW3+A5Juc/iTwuJjCdoKUlY/3t7AkIds7BhHT7jnvO89sqPzcGfkZwQXQl4zT3RhsmSqkXi6Skd1qPEsjXv5VyjEKJHCaXMd2WqvJa5ORiTx74kEdLxMBk; x-main="dTPGR1EUeOl0rlwt6KYxgWgy@y79zqGILIqaEwhJZJFeO8DwqwiEUwoJHIt9cdD1"; at-main=Atza|IwEBIOYwB3Vw6gSC-9mrn55LRLkEb0EM6phy53Pf-yBPU-gIp3NpvkPqV66XMcrULpL-D95n_V0K8y3utdhHU-Kjsycc-hGCVuiOZeV5ITbGjdXoCHzjAj5ykd8_Ydu1XQPf0zaZQoDpCuxljuuNhAKxAdkQxHU_9AC1PwXMKX0IFM1jmUqQhFI8HBd1Pfdrs9mV669wPW8zRwN2ETDIbBOqLQAS_gDLtXke7rpW5F3Bep_DQw; sess-at-main="ZDpcJt3EExmE+bz3zjzLyOFjPxJEGGF6IL0BaaOuPHU="; sst-main=Sst1|PQFl5t7iExTeRMvRjVqBF5t-CSMhh_H1suyQH_6sq0wbPbSp7fL5y2iGCApDjRauZFPSdgzV6epj77x_BnUAfaxnLK6k31ZhTLJbYVTgj-gexZtRFpuqc8XJ_4erx_DODYlWYnyXg_L1G5tVXNu-KXIrPz8m9DstbqGn6soXNdt3A1LxOhi1rpJdYBxgDVRoULtQQQBCw2akA6CcIXo5vLkVO_C1V1cTaxat6lw2rpxLVHIb6oshSnFoIlxiub9IkVkBbQlH1VbGR6RmGidkf9-B42sIQL9vtWI-IiCNVGzdNjE',
+    'referer': 'https://sellercentral.amazon.com/revcal?ref=RC1',
     'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
@@ -32,7 +33,7 @@ headers = {
 }
 
 params = {
-    'searchKey': 'B0187HZG2E',
+    'searchKey': 'B0767TX5PD',
     'countryCode': 'CA',
     'locale': 'en-US',
 }
@@ -43,5 +44,3 @@ response = requests.get(
     cookies=cookies,
     headers=headers,
 )
-
-print(response.json())
