b2638d199abbe217fc2dcbdc6df9d7297602d9bc
farid <Muhammad Farid Saleh>
2023-06-29 02:29:44 +0900

    update script

--- main.py
+++ main.py
@@ -1407,7 +1407,9 @@ if __name__ == "__main__":
 	if platform == "linux" or platform == "linux2":
 		PYLOC = "python"
 	elif platform == "win32":
-		PYLOC = "python.exe"
+		# PYLOC = "python.exe"
+		PYLOC = "C:\synergy-app2\synergy-venv\Scripts\python.exe"
+
 	
 	isExist = os.path.exists("setting.json")
 	if not isExist:
