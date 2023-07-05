1513e547c385588b91986bb2ba48c51f06999b5d
githubfarid1 <github.farid1@gmail.com>
2023-07-05 17:35:20 +0900

    update script

--- modules/scrapebyseller.py
+++ modules/scrapebyseller.py
@@ -76,6 +76,7 @@ def parse(fileinput, profile):
                 try:
                     linkdom = soup.find("div", {"id":"seller-info-storefront-link"}).find("a", class_="a-link-normal")
                     domain = urlparse(url).netloc
+                    input("{}{}".format(domain, linkdom['href']))
                     driver.get("{}{}".format(domain, linkdom['href']) )
                     html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
                     soup = BeautifulSoup(html,"html.parser")
