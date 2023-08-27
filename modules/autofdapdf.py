69558cdebe40c6313483915e442505f5501a7438
muhammad farid <githubfarid1@gmail.com>
2023-08-27 12:16:48 +0900

    update scripts

--- modules/autofdapdf.py
+++ modules/autofdapdf.py
@@ -154,7 +154,7 @@ def webentry_update_individual(pdffile,  pdffolder, item):
     print("Update Web Entry Identification Started..")
     time.sleep(1)
     delimeter = file_delimeter()
-    doc = fitz.open(pdffolder + delimeter + pdffile)
+    doc = fitz.open(pdffile)
     page = doc[0]
     submitter = page.get_text("block", clip=[100.6500015258789, 271.04034423828125, 185.60845947265625, 283.09893798828125]).strip()
     entry_id = page.get_text("block", clip=(152.7100067138672, 202.04034423828125, 230.7493438720703, 214.09893798828125)).strip()
