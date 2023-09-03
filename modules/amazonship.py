f5606a45e949187e654cd0f7757040d6d2eb1f0a
muhammad farid <githubfarid1@gmail.com>
2023-09-03 18:31:01 +0900

    update scripts

--- modules/amazonship.py
+++ modules/amazonship.py
@@ -455,9 +455,9 @@ class AmazonShipment:
             explicit_wait()
             todays_date = date.today()
             todays_str = "{}/{}/{}".format(str(todays_date.month), str(todays_date.day), str(todays_date.year))
+            input("pause")
             dateinput = self.driver.find_element(By.CSS_SELECTOR, "kat-date-picker[id='sendByDatePicker']").find_element(By.CSS_SELECTOR, "input")
-            if not dateinput:
-                input("pause")
+                
             dateinput.clear()
             dateinput.send_keys(todays_str)
             explicit_wait()
