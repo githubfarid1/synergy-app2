6c57bdb1f9463095b048faedd43393eec60dd3ae
muhammad fardan <githubfarid1@gmail.com>
2023-09-25 10:53:04 +0900

    update scripts

--- modules/amazon_screenshot.py
+++ modules/amazon_screenshot.py
@@ -83,6 +83,7 @@ def main():
     xlbook = xw.Book(args.xlsinput)
     xlsheet = xlbook.sheets[args.sheetname]
     box_grouped = data_generator(xlsheet=xlsheet)
+    screenshot(box_grouped, args.chromedata)
     input("End Process..")    
 
 
