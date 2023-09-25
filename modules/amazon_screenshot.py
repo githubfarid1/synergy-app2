56246eb3e2d3ae7e2e4fdc78a5f86266766394d6
muhammad fardan <githubfarid1@gmail.com>
2023-09-25 20:38:34 +0900

    update scripts

--- modules/amazon_screenshot.py
+++ modules/amazon_screenshot.py
@@ -65,7 +65,7 @@ def screenshot(list, chrome_data, filepath):
                 # driver.save_screenshot(filename=filepathsave)
                 # print(filepathsave)
                 # page.insert_image(fitz.Rect(50,50,820,500),filename=filepathsave)
-                input(filepathsave)
+                input(r"" + filepathsave)
                 element = driver.find_element(By.XPATH, '//*[@id="ppd"]')
                 img_url = ob.get_element(driver, element, save_path=filepathsave)
                 print(img_url)
