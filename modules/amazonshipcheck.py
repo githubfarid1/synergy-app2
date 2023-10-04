743e7fe3a75c5019ab247689d1791dc328b4c6fa
muhammad fardan <githubfarid1@gmail.com>
2023-10-04 21:21:19 +0900

    update scripts

--- modules/amazonshipcheck.py
+++ modules/amazonshipcheck.py
@@ -402,7 +402,7 @@ class AmazonShipmentCheck:
                     input(xlssku + " Not Found or internet error. Please check the SKU and run the script again!")
                     sys.exit()
 
-                breakpoint()
+                # breakpoint()
                 # if individual.text.find('Individual units') == -1:
                 if individual.find_element(By.CSS_SELECTOR, "kat-option[value='1']").get_attribute("aria-selected") == "false":
                     individual.click()
