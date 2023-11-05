2e791db3f04da534532cdd73138171a6bee6c07d
Muhammad Farid Saleh <github.farid1@gmail.com>
2023-11-05 11:37:38 +0900

    update scripts

--- tester/amazon_ss.py
+++ tester/amazon_ss.py
@@ -47,7 +47,7 @@ filename = 'file1.png'
 element = driver.find_element(By.XPATH, '//*[@id="ppd"]')
 filepathsave = ob.get_element(driver, element, save_path=r"".join(filepath),image_name=filename)
 
-c = canvas.Canvas(os.path.join(filepath, "file1.pdf"), pagesize=landscape)
+c = canvas.Canvas(os.path.join(filepath, "file1.pdf"), pagesize=letter)
 img = ImageReader(os.path.join(filepath, "file1.png"))
 c.drawImage(img, 0, 0, width=595, height=842)
 
