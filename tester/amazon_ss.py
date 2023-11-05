48008d7445338f3437f904be2dff3596a0461c94
Muhammad Farid Saleh <github.farid1@gmail.com>
2023-11-05 13:17:53 +0900

    update scripts

--- tester/amazon_ss.py
+++ tester/amazon_ss.py
@@ -60,8 +60,10 @@ filepathsave = ob.get_element(driver, element, save_path=r"".join(filepath),imag
 c = canvas.Canvas(os.path.join(filepath, "file1.pdf"), pagesize=landscape(letter))
 
 img = ImageReader(os.path.join(filepath, "file1.png"))
+width = 400
 iw, ih = img.getSize()
-c.drawImage(img, 0, 0, width=iw, height=ih)
+aspect = ih / float(iw)
+c.drawImage(img, 0, 0, width=width, height=(width * aspect))
 
 c.save()
 # opdf = PdfWriter()
